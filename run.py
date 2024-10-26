b731878f9e183564ad8bbd865ff5cad1975549ef
arugo11 <al23088@shibaura-it.ac.jp>
2024-10-26 13:42:12 +0900

    add:herokuデプロイ用に設定ファイルを記述

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-#run.py
-from src.prism  import create_app
+import os
+from src.prism import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
